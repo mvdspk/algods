b1099e4f9b049491b446ae5d3e2392111dcf69b2
mvdspk <123mvds@gmail.com>
2021-04-06 22:39:03 -0700

    data more names for algo ds

--- app.js
+++ app.js
@@ -4,6 +4,22 @@ const datastructureNames = [
   "double linked list",
   "min heap",
   "max-heap",
+  "circular linked list",
+  "BST Binary search tree",
+  "AVL tree",
+  "Red black Tree",
+  "B tree",
+  "graph",
+  "tree",
+  "hash table",
+  "hash map",
+  "tree map",
+  "priority queue",
+  "heap",
+  "queue",
+  "stack",
+  "set",
+  "binary tree",
 ];
 const algorithmNames = [
   "quick sort",
@@ -13,6 +29,12 @@ const algorithmNames = [
   "shell sort",
   "merge sort",
   "heap sort",
+  "divide and conquer",
+  "back tracking",
+  "recursion",
+  "binary search",
+  "DFS Depth first search",
+  "BFS breadth first serach",
 ];
 
 // view
@@ -25,7 +47,7 @@ function renderDataStructureNames() {
   const dsBody = document.querySelector(".data-structure-names .section-body");
   const dsCount = document.querySelector(".data-structure-names .count");
   let html = "";
-  datastructureNames.forEach((dsName) => {
+  datastructureNames.sort().forEach((dsName) => {
     html += `<div class="data-structure-name"> ${dsName}</div>`;
   });
 
@@ -38,11 +60,11 @@ function renderAlgorithmNames() {
   const dsBody = document.querySelector(".algorithm-names .section-body");
   const dsCount = document.querySelector(".algorithm-names .count");
   let html = "";
-  algorithmNames.forEach((dsName) => {
+  algorithmNames.sort().forEach((dsName) => {
     html += `<div class="algorithm-name"> ${dsName}</div>`;
   });
 
   dsBody.innerHTML = html;
 
-  dsCount.innerHTML = datastructureNames.length;
+  dsCount.innerHTML = algorithmNames.length;
 }
